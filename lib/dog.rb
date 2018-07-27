b229bd7023bf70c97ee90077b5cc3eda05b2c97e
KaumudiMummadi <kaumudi.mummadi@gmail.com>
2018-07-27 19:12:27 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,18 @@
 # dog.rb
 class Dog 
-  
-    def initialize(name)
-     @name = name 
-  end 
-  
-  def name= (name)
+
+  def name=(name)
     @name = name 
   end 
   
   def name 
     @name 
   end 
+  
+  def breed=(breed)
+    @breed = breed
 end 
+
 
 fido= Dog.new ("fido")
  puts "My dog's name is #{@name}"
