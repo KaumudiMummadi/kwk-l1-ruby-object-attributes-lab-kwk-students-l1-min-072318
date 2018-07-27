3a445e73494cff8473388c3c02437bc3a44d5fdf
KaumudiMummadi <kaumudi.mummadi@gmail.com>
2018-07-27 18:55:17 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 # dog.rb
 class Dog 
+  def initialize (name, name1)
+    @name= name 
+    @name1= name1
+  end 
   
+  def name = (name)
+    @name = name 
+  end 
+  
+  def name 
+    @name 
+  end 
+  
+mia = Dog.new 
